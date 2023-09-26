bcafe98814488e448fbe87d873203cb67d2a72f3
Abajit Dey <abajitdeyag10118@gmail.com>
2023-09-26 16:58:55 +0530

    Added google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,18 @@
 // Supports weights 100-900
 import "@fontsource-variable/inter";
-import '../styles/global.css'
+import "../styles/global.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-9QTNRHVRN8"
+        ></script>
+        <script src="./index.js"></script>
+      </head>
       <body className="h-full bg-red-400">{children}</body>
     </html>
   );
