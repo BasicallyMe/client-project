9c82dfb9c9d813bc0268e4856c6e589722d78713
Abajit Dey <abajitdeyag10118@gmail.com>
2023-09-26 17:20:08 +0530

    GA4 tag script

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,27 +1,26 @@
 // Supports weights 100-900
 import "@fontsource-variable/inter";
 import "../styles/global.css";
+import Script from "next/script";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        {/* <!-- Google tag (gtag.js) --> */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-9QTNRHVRN8"
-        ></script>
-        <script>
-          {`
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+      <body className="h-full bg-red-400">
+        <div>
+          <Script src="https://www.googletagmanager.com/gtag/js?id=G-9QTNRHVRN8"></Script>
+          <Script id="google-analytics">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
 
-  gtag('config', 'G-9QTNRHVRN8');
-`}
-        </script>
-      </head>
-      <body className="h-full bg-red-400">{children}</body>
+              gtag('config', 'G-9QTNRHVRN8');
+            `}
+          </Script>
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
